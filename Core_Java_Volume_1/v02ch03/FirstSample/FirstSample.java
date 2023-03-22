657107f38fa760b0c0d5837765540d5101a7f487
“Trying_everything” <mwq1742801515@163.com>
2023-03-22 12:29:15 +0800

    更改文件夹

--- Core_Java_Volume_1/v02ch03/FirstSample/FirstSample.java
+++
@@ -1,9 +0,0 @@
-package Core_Java_Volume_1.v02ch03.FirstSample;
-
-public class FirstSample 
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("We will not use 'Hello, World'");
-    }
-}
