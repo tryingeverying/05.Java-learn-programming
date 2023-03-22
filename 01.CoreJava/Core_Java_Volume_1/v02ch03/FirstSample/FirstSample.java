bb2236f6066d773ac038d1856e24244a4995c350
“Trying_everything” <mwq1742801515@163.com>
2023-03-22 12:33:23 +0800

    上传java核心技术第一章

---
+++ 01.CoreJava/Core_Java_Volume_1/v02ch03/FirstSample/FirstSample.java
@@ -0,0 +1,9 @@
+package Core_Java_Volume_1.v02ch03.FirstSample;
+
+public class FirstSample 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("We will not use 'Hello, World'");
+    }
+}
