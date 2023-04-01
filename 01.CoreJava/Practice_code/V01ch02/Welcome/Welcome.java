bfd6e055f7d8214bcb07a8529db37ddfdecfaee6
“Trying_everything” <mwq1742801515@163.com>
2023-04-01 20:01:00 +0800

    添加忽略名单

--- 01.CoreJava/Practice_code/V01ch02/Welcome/Welcome.java
+++ 01.CoreJava/Practice_code/V01ch02/Welcome/Welcome.java
@@ -7,7 +7,7 @@ public class Welcome
 {
     public static void main(String[] args)
     {
-        String greeting = "welcome to core java";
+        String greeting = "welcome to core java !";
         System.out.println(greeting);
         for (int i = 0; i < greeting.length(); i++)
             System.out.print("=");
